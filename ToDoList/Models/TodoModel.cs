3b26726490d8d864d4bfb3efa29bf270988d5dec
Alexandr <toropetsky@yandex.ru>
2021-03-08 19:55:59 +0300

    Добавьте файлы проекта.

---
+++ ToDoList/Models/TodoModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models
+{
+    class TodoModel: INotifyPropertyChanged
+    {
+        // Каждый раз при создании задачи будет считываться время на текущий момент
+        private bool _IsDone;
+        private string _text;
+
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+        public bool IsDone
+        {
+            get { return _IsDone; }
+            set
+            {
+                if (_IsDone == value)
+                    return;
+                _IsDone = value;
+                OnPropertyChanged("IsDone");
+            }
+
+        }
+
+
+        public string Text
+        {
+            get { return _text; }
+            set 
+            {
+                if (_text == value)
+                    return;
+                _text = value;
+                OnPropertyChanged("Text");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));    
+        }
+    }
+    
+}
---
+++ ToDoList/Services/FileIOservice.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoList.Models;
+
+namespace ToDoList.Services
+{
+    class FileIOservice
+    {
+        private readonly  string PATH;
+        public FileIOservice(string path)
+        {
+            PATH = path;
+        }
+        public BindingList<TodoModel> LoadData()
+        {
+            // Проверяем, существует ли файл? 
+            var fileExists = File.Exists(PATH);
+            if (!fileExists)
+            {
+                // Если файла не существует - создаем файл, передает путь 
+                File.CreateText(PATH).Dispose();
+                return new BindingList<TodoModel>();
+            }
+            // Если файл уже существовал, выполняем чтение из файла
+            using (var reader = File.OpenText(PATH))
+            {
+                var fileText = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<BindingList<TodoModel>>(fileText);
+            }
+        }
+        public void SaveData(object todoDataList)
+        {
+            using (StreamWriter writer = File.CreateText(PATH))
+            {
+
+                string output = JsonConvert.SerializeObject(todoDataList);
+                writer.Write(output);
+            }
+            
+        } 
+    }
+}
